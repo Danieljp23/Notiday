800447017908a5c794c3108e64bb428bef38fff3
Danieljp23 <danieljp23@hotmail.com>
2024-03-01 17:19:17 -0300

    Notiday- patch-1
    O aplicativo sofreu mudanças principalmente no appBar, onde foi trocado a formatação por um theme para utilizarmos a elevação do elemnto appBar, para ficar de uma maneira visualmente mais atrativa ao usuário, foi criada a tela de política e termos do aplicativo, e a chamada da mesma através da classe Registrar do aplicativo, foi também utilizado a classe SingleChildScrollView para resolver o problema de overflow que foi gerado pelo teclado que sobrepunha os botões da tela, foi alterado a cor de alguns elementos para tornar um experiência mais atrativa e menos exaustiva ao usuário.

---
+++ lib/registrar.dart
@@ -0,0 +1,214 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:notiday/login.dart';
+import 'package:notiday/main.dart';
+
+class _RegistrarState extends State<Registrar> {
+  bool aceito = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: PreferredSize(
+          preferredSize: Size.fromHeight(kToolbarHeight),
+          child: Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.only(
+                bottomRight: Radius.circular(20),
+                bottomLeft: Radius.circular(20),
+              ),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.3),
+                  spreadRadius: 1,
+                  blurRadius: 10,
+                  offset: Offset(0, 3), // changes position of shadow
+                ),
+              ],
+            ),
+            child: AppBar(
+              title: Container(
+                child: Row(
+                  children: [
+                    IconButton(
+                      icon: Icon(Icons.arrow_back_ios_new),
+                      onPressed: () {
+                        Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (context) => Login(),
+                          ),
+                        );
+                      },
+                      color: Colors.black,
+                    ),
+                    SizedBox(width: 90.0),
+                    Text(
+                      'Notiday',
+                      style: TextStyle(
+                          fontFamily: AutofillHints.birthday,
+                          fontSize: 30,
+                          color: Colors.blue),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+        body: SingleChildScrollView(
+          child: Center(
+            child: Container(
+              width: 600,
+              height: 800,
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 0, vertical: 70),
+                      child: Text(
+                        'Cadastro',
+                        style: TextStyle(
+                            color: Colors.grey,
+                            fontSize: 40,
+                            fontWeight: FontWeight.bold,
+                            fontStyle: FontStyle.italic),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(5.0),
+                      child: TextField(
+                        style: TextStyle(fontSize: 10),
+                        decoration: InputDecoration(
+                          labelText: 'NOME',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(30.0),
+                            borderSide:
+                                BorderSide(color: Colors.black, width: 5.0),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(5.0),
+                      child: TextField(
+                        style: TextStyle(fontSize: 5),
+                        maxLines: 1,
+                        decoration: InputDecoration(
+                          labelText: 'SOBRENOME',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(30.0),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(5.0),
+                      child: TextField(
+                        style: TextStyle(fontSize: 5),
+                        maxLines: 1,
+                        decoration: InputDecoration(
+                          fillColor: Colors.blue,
+                          labelText: 'E-MAIL',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(30.0),
+                            borderSide: BorderSide(color: Colors.black),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(5.0),
+                      child: TextField(
+                        style: TextStyle(fontSize: 5),
+                        maxLines: 1,
+                        decoration: InputDecoration(
+                          labelText: 'TELEFONE',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(30.0),
+                            borderSide: BorderSide(color: Colors.black),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(5.0),
+                      child: TextField(
+                        style: TextStyle(fontSize: 5),
+                        maxLines: 1,
+                        decoration: InputDecoration(
+                          labelText: 'SENHA',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(30.0),
+                            borderSide: BorderSide(color: Colors.black),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(5.0),
+                      child: TextField(
+                        style:
+                            TextStyle(fontSize: 5, fontWeight: FontWeight.bold),
+                        maxLines: 1,
+                        decoration: InputDecoration(
+                          contentPadding: EdgeInsets.symmetric(
+                              vertical: 20, horizontal: 20),
+                          labelText: 'CONFIRMAR SENHA',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(30.0),
+                            borderSide: BorderSide(color: Colors.black),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        Checkbox(
+                          value: aceito,
+                          onChanged: (bool? checked) {
+                            setState(() {
+                              aceito = checked ?? false;
+                            });
+                          },
+                        ),
+                        Text('Concordo com os Termos e Políticas')
+                      ],
+                    ),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Color.fromARGB(255, 243, 166, 0),
+                        foregroundColor: Colors.black,
+                      ),
+                      onPressed: aceito
+                          ? () {
+                              Navigator.of(context).push(
+                                MaterialPageRoute(
+                                  builder: (context) => Notiday(),
+                                ),
+                              );
+                            }
+                          : null,
+                      child: Text(
+                        'Registrar',
+                        style: TextStyle(color: Colors.black, fontSize: 20),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Registrar extends StatefulWidget {
+  _RegistrarState createState() {
+    return _RegistrarState();
+  }
+}
